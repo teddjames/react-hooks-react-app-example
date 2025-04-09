7c5d0403cfdc8082179e911c2597de40abd3c78c
Tedd James <tedd.james@student.moringaschool.com>
2025-04-09 14:24:28 +0300

    A React App Example (CodeGrade)

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent";
 
+
 // Add your code own within the return statement
 function App() {
   return (
